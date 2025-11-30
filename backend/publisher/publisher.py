1d788cac63bea8f5c375b31dd2688aec2c3c5e99
joeD1966 <jneal04@uafs.edu>
2025-11-30 15:50:29 -0600

    revised publisher.py with mapping proc and sys, and stubbed publisher.yml
    To get these up for use - running the entire script has its own issues.

--- backend/publisher/publisher.py
+++ backend/publisher/publisher.py
@@ -2,48 +2,92 @@ import os
 import time
 import random
 import paho.mqtt.client as mqtt
-import adafruit_dht
-import board
+import json
+import sys
+
+# --------------------------- 
+# REMAP PROC ND SYS FOR DOCKER
+# ---------------------------
+
+PROCFS = os.getenv("PROCFS_MOUNT", "/proc")
+SYSFS = os.getenv("SYSFS_MOUNT", "/sys")
+
+# Override paths so hardware detection libraries work
+os.environ["BLINKA_FIRMWARE_DETECT"] = "1"
+os.environ["BLINKA_PROC"] = PROCFS
+os.environ["BLINKA_SYS"] = SYSFS
+
+# Force Adafruit PlatformDetect to use our remapped paths
+sys.path.insert(0, "/usr/local/lib/python3.9/site-packages")
+
+# ---------------------------
+# NORMAL PROGRAM IMPORTS
+# ---------------------------
+import RPi.GPIO as GPIO
+import dht11
+
+# ---------------------------
+# INIT
+# ---------------------------
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+DHT_PIN = 4  # or whatever you would use on a real Pi
+dht11sensor = dht11.DHT11(pin=DHT_PIN)
 
 MQTT_HOST = os.getenv("MQTT_HOST", "mqtt-broker")
 MQTT_PORT = int(os.getenv("MQTT_PORT", "1883"))
 
-try:
-    import adafruit_dht
-    HAS_DHT = True
-except ImportError:
-    adafruit_dht = None
-    HAS_DHT = False
-
-DHT_PIN = 4  # or whatever you would use on a real Pi
+MQTT_HOST = "192.168.1.9"
+MQTT_PORT = 1883
 
 client = mqtt.Client()
 
 def connect():
     client.connect(MQTT_HOST, MQTT_PORT, 60)
+# 30C - 50C at idle to 60C - 75°C under load
+# cooling fan 23-45C
 
+def read_cpu_temp():
+    try:
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+            temp_str = f.readline().strip()
+            # millidegrees Celsius
+            celsius = float(temp_str) / 1000.0
+            
+            return celsius
+    except FileNotFoundError:
+        return None
+        
+def celsius_to_farenheight(celsius):
+	farenheight = float((9 * celsius/5) + 32)
+	return farenheight
+	
 def read_sensors():
-    if HAS_DHT:
-        sensor = adafruit_dht.DHT11(board.D22)
-        humidity, temperature =sensor.humidity, sensor.temperature
-        if humidity is None or temperature is None:
-            # fallback if sensor read fails
-            humidity = random.uniform(30, 60)
-            temperature = random.uniform(20, 30)
-    else:
-        # running on PC / no Adafruit_DHT -> fake data
-        humidity = random.uniform(30, 60)
-        temperature = random.uniform(20, 30)
+	result = dht11sensor.read()
+	if result.is_valid():
+		humidity = result.humidity
+		temperature = result.temperature
+	else:
+		# running on PC / no Adafruit_DHT -> fake data
+		# fallback if sensor read fails
+		humidity = random.uniform(30, 60)
+		temperature = random.uniform(20, 30)
 
-    cpu_temp = random.uniform(40, 60)  # TODO: replace with real CPU temp read if you want
-    return temperature, humidity, cpu_temp
+	cpu_temp = read_cpu_temp()
+	if cpu_temp == None:
+		random.uniform(40, 60)  # TODO: fall back
+	return temperature, humidity, cpu_temp
 
 def main():
     time.sleep(5)
     connect()
     while True:
         temp, hum, cpu = read_sensors()
-        msg - f"{temp},{hum},{cpu}"
+        temp = round(temp,2)
+        hum = round(hum,2)
+        cpu = round(cpu,2)
+        msg = f"{temp},{hum},{cpu}"
         client.publish("sensors/data", msg)
         time.sleep(5)
 
